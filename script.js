427d25db6d003a723720c1ae2c8005287e7be303
epap011 <papageorgiou.efthimis@proton.me>
2025-04-14 19:22:20 +0300

    feat: update styles

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let slides         = [];
 let currentSlide   = 0;
-const interval     = 10000;
+const interval     = 5000;
 const fadeDuration = 1000;
 
 const slideContainer = document.getElementById('slide-container');
